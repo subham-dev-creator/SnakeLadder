150ad80d3d2b5feb3875f19b88297a16eb8f0d5f
subhamstark <subhamofficial98@gmail.com>
2020-09-23 14:40:08 +0530

    Project Setup

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,12 +1,12 @@
 
 public class SnakeLadder {
 		
-	static final int startPos=0;
-	static final int maxPlayer=1;
+//	static final int startPos=0;
+//	static final int maxPlayer=1;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Snake & Ladder game");
 	}
 
 }
