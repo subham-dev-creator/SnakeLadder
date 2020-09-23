98c9e0bd64f814831a9e59ecc83835a6a34f97a5
subhamstark <subhamofficial98@gmail.com>
2020-09-23 14:50:21 +0530

    UC2 Done

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -3,10 +3,16 @@ public class SnakeLadder {
 		
 	static final int startPos=0;
 	static final int maxPlayer=1;
-	
+	static final int DiceRange=6;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int CurrentDiceNumber = (int) (Math.random()*20%6);
+		//as mod with 6 can generate 0 so to over come it we increament it
+		CurrentDiceNumber++;
+		
+		
+		System.out.println("Current Dice : " + CurrentDiceNumber);
+		
 	}
 
 }
