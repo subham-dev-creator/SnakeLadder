2f51b910179c0ea27ac8e8930ad864188119abd1
subhamstark <subhamofficial98@gmail.com>
2020-09-23 22:47:29 +0530

    UC7 Done

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -8,37 +8,53 @@ public class SnakeLadder {
 	static final int MaxPos=100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int NumOfPlayers=2;
+		int[] CurPosOfPlayers = new int[NumOfPlayers];
+		
+		Boolean IsWin=false;
+		int Winner=-1;
 		int DiceRoll=0;
-		int currentPos=startPos;
-		while(currentPos<MaxPos) {
-			if(currentPos<0)
-				currentPos=startPos;
-			
-			int Option=(int)(Math.random()*20 %3);
-			int CurrentDiceNumber = (int) (Math.random()*20%6);
-			DiceRoll++;
-			//as mod with 6 can generate 0 so to over come it we increament it
-			CurrentDiceNumber++;
-			
-			
-			if(Option==0) {
-				//no play
+		
+		for(int i=0;i<NumOfPlayers;i++)
+			CurPosOfPlayers[i]=startPos;
+		
+		while(IsWin==false) {
+			for(int i=0;i<NumOfPlayers;i++) {
+				if(CurPosOfPlayers[i]<0)
+					CurPosOfPlayers[i]=startPos;
 				
-			}
-			else if(Option==1) {
-				//Moves Ahead
-				currentPos+=CurrentDiceNumber;
-				if(currentPos>100)
-					currentPos-=CurrentDiceNumber;
+				int Option=(int)(Math.random()*20 %3);
+				int CurrentDiceNumber = (int) (Math.random()*20%6);
+				DiceRoll++;
+				//as mod with 6 can generate 0 so to over come it we increament it
+				CurrentDiceNumber++;
 				
+				
+				if(Option==0) {
+					//no play
+					
+				}
+				else if(Option==1) {
+					//Moves Ahead
+					CurPosOfPlayers[i]+=CurrentDiceNumber;
+					if(CurPosOfPlayers[i]==100)
+					{
+						IsWin=true;
+						Winner=i;
+						break;
+					}
+					if(CurPosOfPlayers[i]>100)
+						CurPosOfPlayers[i]-=CurrentDiceNumber;
+					
+				}
+				else {
+					//moves back;
+					CurPosOfPlayers[i]-=CurrentDiceNumber;
+				}
+				System.out.println("Current Player is " + i +" Current Pos : "+CurPosOfPlayers[i]);
 			}
-			else {
-				//moves back;
-				currentPos-=CurrentDiceNumber;
-			}
-			System.out.println("Current Pos : "+currentPos);
 		}
-		System.out.println("Win ");
+		System.out.println("Win is "+Winner);
 		System.out.println("Total Dice Roll : "+DiceRoll);
 	}
 
