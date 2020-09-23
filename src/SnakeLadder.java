9529e0f41e3785a10e5438a34a378951fd1d78e5
subhamstark <subhamofficial98@gmail.com>
2020-09-23 14:36:51 +0530

    intial Commit

---
+++ src/SnakeLadder.java
@@ -0,0 +1,12 @@
+
+public class SnakeLadder {
+		
+	static final int startPos=0;
+	static final int maxPlayer=1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
